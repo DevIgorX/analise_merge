5cc467c82d831668b690fa18735c64ff08636e2d
Tauam Igor <tauamigor@gmail.com>
2025-12-30 15:00:10 -0300

    ajustando script para limpar colunas dos dataframe

--- analise.py
+++ analise.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 df_preventiva = pd.read_excel('PREVENTIVA.xlsx')
 df_carreta = pd.read_excel('CARRETA.xlsx')
+df_carreta.columns = df_carreta.columns.str.strip()
 df_Esl = pd.read_excel('ESL.xlsx')
 df_mobile = pd.read_excel('MOBILE.xlsx')
 df_bipe = pd.read_excel('BIPE.xlsx')
 df_bipe_de_notas = pd.read_excel("BIPE DE NOTAS.xlsx")
+df_bipe_de_notas.columns = df_bipe_de_notas.columns.str.strip()
 
-df_merge = pd.merge(df_preventiva, df_carreta[['PEDIDO','NF`s','CHAVE','PREVISÃO ENTREGA']], left_on='PEDIDO 1P/FULL', right_on='PEDIDO', how='left').drop(columns=['PEDIDO'])
+
+df_merge = pd.merge(df_preventiva, df_carreta[['PEDIDO','NF`s','CHAVE','PREVISÃO ENTREGA', 'TIPO']], left_on='PEDIDO 1P/FULL', right_on='PEDIDO', how='left').drop(columns=['PEDIDO'])
 
 df_merge_2 = df_merge.merge(df_mobile[['Pedido','Tipo','Entregador']], left_on='PEDIDO 1P/FULL', right_on='Pedido', how='left').drop(columns=['Pedido'])
 
@@ -15,6 +18,6 @@ df_merge_3 = pd.merge(df_merge_2,df_Esl[[ 'Nota Fiscal/Chave NF-e','Última ocor
 
 df_merge_4 = pd.merge(df_merge_3, df_bipe[['PEDIDO','INFO']], left_on='PEDIDO 1P/FULL', right_on='PEDIDO', how='left').drop(columns=['PEDIDO'])
 
-df_merge_5 = pd.merge(df_merge_4, df_bipe_de_notas[['NF', 'OCORRENCIA ']], left_on='NF`s', right_on='NF', how='left').drop(columns=['NF'])
+df_merge_5 = pd.merge(df_merge_4, df_bipe_de_notas[['NF', 'OCORRENCIA']], left_on='NF`s', right_on='NF', how='left').drop(columns=['NF'])
 
 df_merge_5.to_excel('29-12-2025.xlsx', sheet_name='Relatorio_diario', index=False)
