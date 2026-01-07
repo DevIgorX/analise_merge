5b684b594eab99a5166f5ec13e1a3c9a271d19a0
tauam <tauam.pinheiro@goias.gov.br>
2026-01-07 16:38:50 -0300

    alteração no script analise.py

--- analise.py
+++ analise.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 
 df_preventiva = pd.read_excel('Preventiva_07-01-2026.xlsx')
@@ -65,6 +66,19 @@ df_final = (
         .drop(columns=['NF'])
 )
 
+df_final_2 = (pd.merge(df_carreta,df_esl[['Nota Fiscal/Chave NF-e', 'Ocorrência/Ocorrência']], left_on='CHAVE', right_on='Nota Fiscal/Chave NF-e', how='left')
+              .drop(columns=['Nota Fiscal/Chave NF-e']))
+
+
+df_final['Última ocorrência/Observações'] = df_final['Última ocorrência/Observações'].fillna('Não informado')
+
+
+df_final_3 = pd.merge(df_final, df_final_2[['CHAVE', 'Ocorrência/Ocorrência']], left_on='CHAVE', right_on='CHAVE', how='left').drop(columns=['CHAVE'])
+
+
+filtro = df_final_3['Última ocorrência/Observações'] == 'Não informado'
+
+df_final_3.loc[filtro, 'Última ocorrência/Observações'] = df_final_3.loc[filtro, 'Ocorrência/Ocorrência']
 
 colunas_datas = [
     'PREVISÃO ENTREGA',
@@ -72,11 +86,14 @@ colunas_datas = [
 ]
 
 for col in colunas_datas:
-    df_final[col] = pd.to_datetime(df_final[col], errors='coerce')
+    df_final_3[col] = pd.to_datetime(df_final_3[col], errors='coerce')
 
+
+data_hoje = datetime.today().strftime('%d-%m-%Y')
+nome_arquivo = f'{datetime}.xlsx'
 
 with pd.ExcelWriter(
-    '29-12-2025.xlsx',
+    nome_arquivo,
     engine='xlsxwriter',
     datetime_format='dd/mm/yyyy'
 ) as writer:
