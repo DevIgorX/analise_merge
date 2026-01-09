add18416030faf5963923d26980a0f38f7edff40
tauam <tauam.pinheiro@goias.gov.br>
2026-01-09 17:40:15 -0300

    corrigindo função

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,5 @@
 from flask import Blueprint, render_template, url_for, request, current_app, flash, redirect
-from werkzeug.utils import secure_filename
 import os
-
-
 
 rotas = Blueprint('rotas', __name__)
 
@@ -21,11 +18,8 @@ def verficar():
         return redirect(url_for('rotas.home'))
     
     upload_path = current_app.config['DIRETORIO_DADOS']
-    nome_arquivo = secure_filename(arquivo.filename) 
 
-    caminho_final = os.path.join(upload_path, nome_arquivo)
-
-    arquivo.save(caminho_final)
+    arquivo.save(os.path.join(upload_path, arquivo.filename))
 
     
     flash('Arquivo salvo com sucesso!')
