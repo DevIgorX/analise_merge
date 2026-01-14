9d819d5266e409952cc77aa3d73eb26222fde7cf
tauam <tauam.pinheiro@goias.gov.br>
2026-01-14 17:41:37 -0300

    adicionando script process

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, url_for, request, current_app, flash, redirect
 import os
+import subprocess
+import json
+import locale
+
 
 rotas = Blueprint('rotas', __name__)
 
@@ -21,6 +25,11 @@ def verficar():
 
     for arquivo in arquivos:
         arquivo.save(os.path.join(pasta_dados, arquivo.filename))
+
+    diretorio_raiz = current_app.config['DIRETORIO_RAIZ']
+    caminho_analise = os.path.join(diretorio_raiz, 'analise')
+
+    encoding_padrao = locale.getdefaultlocale(False)
 
 
 
